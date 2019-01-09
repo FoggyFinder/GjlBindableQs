8c79bd856e5309d024fe6683ed55e18458a83268
FoggyFinder <FoggyFinder@yandex.ua>
2019-01-09 23:49:45 +0200

    add simple fsxaml sample

---
+++ FsXamlSample/Program.fs
@@ -0,0 +1,19 @@
+﻿open FsXaml
+open System
+open System.Windows
+
+type MainWindowBase = XAML<"MainWindow.xaml">
+
+type MainWindow() as self =
+    inherit MainWindowBase()
+
+    do self.submitButton2.Click.Add (fun _ -> MessageBox.Show("Hello world2!") |> ignore)
+
+    override this.submitButton_Click (s: obj, e: RoutedEventArgs) = 
+        MessageBox.Show("Hello world!")
+        |> ignore
+
+[<STAThread>]
+MainWindow()
+|> Application().Run
+|> ignore
